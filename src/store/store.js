d244798f131f604be4305a9f4c0a88e82c178af3
Koylyak Andriy <koylyakandriy@gmail.com>
2020-05-21 16:47:39 +0300

    predeploy

--- src/store/store.js
+++ src/store/store.js
@@ -3,24 +3,24 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./reducers/index";
 
-const initialState = {};
+// const initialState = {};
+//
+// const middleware = [thunk];
 
-const middleware = [thunk];
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(thunk))
 );
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//
-// const store = createStore(
-//   rootReducer,
-//   composeEnhancers(applyMiddleware(thunk))
-// );
 
 export default store;
