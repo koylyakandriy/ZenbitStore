e52f639386b22b24081eda42ea0106bcf10e3795
Andrii Koiliak <akoiliak@softjourn.com>
2020-05-21 16:15:25 +0300

    Updates

---
+++ precache-manifest.8c81ca34040574489bfc3e90681b4090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8151b93a8ccdf2a623dcce5b23a41f2",
+    "url": "/ZenbitStore/index.html"
+  },
+  {
+    "revision": "33f119112bfb4ef7eae0",
+    "url": "/ZenbitStore/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "33f119112bfb4ef7eae0",
+    "url": "/ZenbitStore/static/js/2.24e52aea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ZenbitStore/static/js/2.24e52aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36fa248fab52bd13f11",
+    "url": "/ZenbitStore/static/js/main.aa948ea3.chunk.js"
+  },
+  {
+    "revision": "e31cd465ae2ced50d2dd",
+    "url": "/ZenbitStore/static/js/runtime-main.c154bf0c.js"
+  }
+]);
