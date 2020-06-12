3d71194134b7097248643367e98240f0bcef1a9b
Andriy Koylyak <koylyakandriy@gmail.com>
2020-06-12 09:01:01 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,19 +3,6 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./reducers/index";
 
-// const initialState = {};
-//
-// const middleware = [thunk];
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(...middleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
